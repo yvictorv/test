10b85b6d6b6cd9c87c9ebf0a7b2f3df2f23eb08e
yvictorv <ermolovv9@gmail.com>
2021-10-18 21:51:24 +0300

    First Commit

---
+++ 3_Объекты--основы.js
@@ -0,0 +1,53 @@
+// Объект или комплексный тип данных
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+
+console.log(person.name); // Получение значения первого свойства
+console.log(person["name"]); // Второй способ получения значения свойства
+
+let arr = ['plum.png', 'orange.png', 'apple.bmp'];
+console.log(arr[0]);
+
+
+//GIT 
+//git bash
+// echo >> README.md
+
+//установка имени пользователя и пароля
+// git config --local user.name "name"
+// git config --local user.email example@gmail.com
+
+// Настройка состояний
+// 1 состояние -- файл находится в  проекте и не связан с git
+// 2 состояние -- git следит за изменениями файла
+// 3 состояние -- git отслеживает изменения и если нужно то можно вернуться
+
+// git add -A -- перевод файла с 1 во 2 состояние
+// git status -- посмотреть статус репозитория
+// git commit -a -m "Мои первый коммит" --> перевод с 2 в 3 состояние(-a add, -m message  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
